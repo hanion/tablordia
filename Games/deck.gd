0c7f22f7d163a6c9c546a4c488b8d72fa5f95ca0
han_ion <55713249+han-ion@users.noreply.github.com>
2021-07-10 21:15:55 +0300

    make deck rcm less crowded

--- Games/deck.gd
+++ Games/deck.gd
@@ -238,13 +238,18 @@ func prepare_rcm(popup:PopupMenu) -> void:
 		
 		
 		
-		popup.add_item("Make cards visible",2)
-		if not can_make_cards_visible:
-			popup.set_item_disabled(popup.get_item_index(2),true)
+		if can_make_cards_hidden and can_make_cards_visible:
+			popup.add_item("Make cards visible",2)
+			popup.add_item("Make cards hidden",3)
+			
 		
-		popup.add_item("Make cards hidden",3)
-		if not can_make_cards_hidden:
-			popup.set_item_disabled(popup.get_item_index(3),true)
+#		popup.add_item("Make cards visible",2)
+#		if not can_make_cards_visible:
+#			popup.set_item_disabled(popup.get_item_index(2),true)
+#
+#		popup.add_item("Make cards hidden",3)
+#		if not can_make_cards_hidden:
+#			popup.set_item_disabled(popup.get_item_index(3),true)
 		
 		popup.add_item("Draw",4)
 	
