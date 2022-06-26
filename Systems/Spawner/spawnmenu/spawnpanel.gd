33c20aaffb693bb55930ce25d32a70ce5eed8cd5
han_ion <55713249+han-ion@users.noreply.github.com>
2022-06-26 22:45:10 +0300

    made spawnpanel a WindowDialog

--- Systems/Spawner/spawnmenu/spawnpanel.gd
+++ Systems/Spawner/spawnmenu/spawnpanel.gd
@@ -1,8 +1,9 @@
-extends Panel
+extends Control
 # SpawnPanel
 signal on_menu_opened()
 signal on_menu_closed()
 
+onready var wd = $WindowDialog
 
 func open_menu() -> void:
 	if visible:
@@ -10,19 +11,25 @@ func open_menu() -> void:
 		return
 	
 	visible = true
+	wd.popup_centered()
 	Std.is_blocked_by_ui = true
 	
-	var spawn_button = get_node("vb/mrgn/hs/configurer/Inspector/vb/vsc/SpawnButton")
+	var spawn_button = get_node("WindowDialog/vb/mrgn/hs/configurer/Inspector/vb/vsc/SpawnButton")
 	spawn_button.disabled = not get_tree().is_network_server()
 	emit_signal("on_menu_opened")
 
 
 func close_menu() -> void:
 	visible = false
+	wd.visible = false
 	Std.is_blocked_by_ui = false
 #	get_node("../..").player.get_node("CAM")._lock_movement = false
 	emit_signal("on_menu_closed")
 
 
 func _on_CloseButton_pressed():
+	close_menu()
+
+
+func _on_WindowDialog_popup_hide() -> void:
 	close_menu()
